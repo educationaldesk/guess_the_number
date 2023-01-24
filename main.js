49d02df3d18a9391eff90640ffb2f2435ec4b63c
Abhishek <69632868+educationaldesk@users.noreply.github.com>
2023-01-24 15:14:55 +0000

    created variable
    created variables for the base version of the game

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// button variables
+const selectLvl = document.querySelector(".lvl"); //to be used for levels
+const check = document.querySelector(".check");
+const again = document.querySelector(".again");
+
+/* displays from DOM*/
+const maxValForGuess = document.querySelector(".maxVal"); //for levels
+const mainDisplay = document.querySelector(".main_display");
+
+//score displays
+const highestScore = document.getElementById("highestScore");
+const yourScore = document.getElementById("yourScore");
+const chancesLeft = document.getElementById("chancesLeft");
